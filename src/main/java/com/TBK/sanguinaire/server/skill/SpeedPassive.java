7c974e8afa838062738bc8a1dbadb091e8d7b082
SangreBK <SangreBK@gmail.com>
2024-09-29 13:19:21 -0300

    update

--- src/main/java/com/TBK/sanguinaire/server/skill/SpeedPassive.java
+++ src/main/java/com/TBK/sanguinaire/server/skill/SpeedPassive.java
@@ -6,7 +6,7 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 public class SpeedPassive extends PassiveAbstract{
     public SpeedPassive() {
         super("transform_bat", 0);
-        this.addAttributeModifier(Attributes.MOVEMENT_SPEED,"91AEAA56-376B-4498-935B-2F7F68070635", (double)0.1F, AttributeModifier.Operation.MULTIPLY_TOTAL);
+        this.addAttributeModifier(Attributes.MOVEMENT_SPEED,"91AEAA56-376B-4498-935B-2F7F68070635", (double)0.08F, AttributeModifier.Operation.MULTIPLY_TOTAL);
     }
 
 }
